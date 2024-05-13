98c1899af183c0dd71d6142a5409d0ff7dce9a55
kolbbond <kolbbond@gmail.com>
2024-05-13 19:24:20 -0400

    add imgui

---
+++ src/Magnum/ImPlotIntegration/Context.hpp
@@ -0,0 +1,16 @@
+#ifndef Magnum_ImPlotIntegration_Context_hpp
+#define Magnum_ImPlotIntegration_Context_hpp
+
+#include "Magnum/ImPlotIntegration/visibility.h" /* defines IMGUI_API */
+
+#include <implot.h>
+#include <Corrade/Utility/Resource.h>
+
+#include "Magnum/ImPlotIntegration/Context.h"
+
+namespace Magnum { namespace ImPlotIntegration {
+
+
+}}
+
+#endif
---
+++ src/MagnumExternal/clone_repos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone git@github.com:ocornut/imgui.git ImGui
+git clone git@github.com:epezent/implot.git ImPlot
+
